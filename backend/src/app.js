1e70bf56ef4dd8932ebb88e4928100cd8657df67
Akash Vijayan <akash.it2027@gmail.com>
2026-08-04 15:13:09 +0530

    backed updating...

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const healthRoutes = require("./routes/health.routes");
 
 const app = express();
-
+const authRoutes = require("./routes/auth.routes");
 // Middleware
 app.use(cors());
 app.use(helmet());
@@ -15,5 +15,5 @@ app.use(express.json());
 
 // Routes
 app.use("/api/health", healthRoutes);
-
+app.use("/api/auth", authRoutes);
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,58 @@
+// require("dotenv").config();
+
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// const PORT = process.env.PORT || 5000;
+
+// const startServer = async () => {
+//     try {
+//         // Connect Database
+//         await connectDB();
+
+//         // Start Server
+//         app.listen(PORT, () => {
+//             console.log("=================================");
+//             console.log(" CloudWatchX Backend Started");
+//             console.log("=================================");
+//             console.log(` Server running on Port ${PORT}`);
+//             console.log(` Health Check: http://localhost:${PORT}/api/health`);
+//         });
+//     } catch (error) {
+//         console.error("Server Failed to Start");
+//         console.error(error.message);
+//     }
+// };
+
+// startServer();
+
 require("dotenv").config();
 
+console.log("Step 1: Environment Loaded");
+
 const app = require("./app");
+console.log("Step 2: App Loaded");
+
 const connectDB = require("./config/db");
+console.log("Step 3: DB Module Loaded");
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
-        // Connect Database
+        console.log("Step 4: Connecting to MongoDB...");
         await connectDB();
 
-        // Start Server
+        console.log("Step 5: Starting Express Server...");
+
         app.listen(PORT, () => {
             console.log("=================================");
-            console.log(" CloudWatchX Backend Started");
+            console.log("CloudWatchX Backend Started");
             console.log("=================================");
-            console.log(` Server running on Port ${PORT}`);
-            console.log(` Health Check: http://localhost:${PORT}/api/health`);
+            console.log(`Server running on Port ${PORT}`);
         });
     } catch (error) {
-        console.error("Server Failed to Start");
-        console.error(error.message);
+        console.error(error);
     }
 };
 
